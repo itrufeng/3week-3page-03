b2790492c2930542063fb681b9c3a3f38939aa83
Jian Zhang <itrufeng@gmail.com>
2017-09-21 17:40:14 +0800

    rework for now

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
+const watch = require('gulp-watch');
 
 gulp.task('serve', ['sass'], function() {
 });
 
 gulp.task('sass', function() {
     return gulp.src(['styles/*.scss', 'styles/*.css'])
+        .pipe(watch('styles/*.scss'))
         .pipe(sass())
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('dist'));
